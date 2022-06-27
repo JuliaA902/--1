afd55102e638c45334ccfa99569b6c0828086afd
iMac <89259058289@mail.ru>
2022-06-27 21:05:46 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и выдает, какое число большее, а какое меньшее.
+Console.Write("Ведите первое число => ");
+string paramstring1 = Console.ReadLine();
+int numberInt1 = int.Parse(paramstring1);
+Console.Write("Введите второе число => ");
+string paramstring2 = Console.ReadLine();
+int numberInt2 = int.Parse(paramstring2);
+int max = 0, min = 0;
+
+if (numberInt1 > numberInt2)
+{
+    max = numberInt1;
+    min = numberInt2;
+    Console.WriteLine($"Первое число является большим: {max}");
+}
+if (numberInt1 < numberInt2)
+{
+    max = numberInt2;
+    min = numberInt1;
+    Console.WriteLine($"Второе число является большим: {max}");
+}
+else (numberInt1 = numberInt2);
+{
+    Console.WriteLine("Данные числа равны");
+} 
---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Введите первое число => ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+if (numberInt%2==0)
+{
+    Console.WriteLine($"Число {numberInt} будет являться четным");
+}
+else
+{
+    Console.WriteLine($"Число {numberInt} будет являться нечетным");
+}
